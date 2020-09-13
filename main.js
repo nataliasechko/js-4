f947a8713ebacd166450692e13d89e9db392c1b4
nataliasechko <52995515+nataliasechko@users.noreply.github.com>
2020-09-13 21:02:37 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,38 @@
 function clearLeft() {
   const divLeft = document.querySelectorAll(".label-area")[0];
   const textElement = divLeft.querySelector("textarea");
-
   divLeft.removeChild(textElement);
-
   const newTextarea = document.createElement("textarea");
   divLeft.appendChild(newTextarea);
 }
 
 function convertText() {
   const pEl = document.createElement("p");
-
   const something = document.getElementById("textarea").value;
-
   pEl.innerHTML = something;
-
   const parentElement = document.querySelectorAll("div.right-text span")[0];
-
   if (!parentElement) {
     return;
   }
-
   parentElement.appendChild(pEl);
 }
 
 function clearRight() {
   const divRight = document.querySelectorAll("div.right-text")[0];
   const spanElement = divRight.querySelector("span");
-
   divRight.removeChild(spanElement);
-
   const newSpanRight = document.createElement("span");
   divRight.appendChild(newSpanRight);
 }
 
-function changecolor() {
-  const changeText = document.querySelectorAll("div.right-text span").value[0];
-  changeText.classLisy.add("blue");
+function colorBlue() {
+  const divRight = document.querySelectorAll("div.right-text")[0];
+  const spanElement = divRight.querySelector("span");
+  spanElement.classList.add('blue'); 
+}
+
+function colorRed() {
+  const divRight = document.querySelectorAll("div.right-text")[0];
+  const spanElement = divRight.querySelector("span");
+  spanElement.classList.add('red');   
 }
